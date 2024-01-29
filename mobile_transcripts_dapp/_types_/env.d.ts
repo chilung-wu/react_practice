14367385a7113734ce1daae27f65576dd63679ed
chilung <chilung.wu.cs10@nycu.edu.tw>
2024-01-29 17:00:25 +0800

    mobile app, react-native init, get info from .env

---
+++ mobile_transcripts_dapp/_types_/env.d.ts
@@ -0,0 +1,9 @@
+declare module '@env' {
+    export const SEPOLIA_PRIVATE_KEY: string;
+    export const INFURA_API_KEY: string;
+    export const DEPLOY_ADDRESS: string;
+    export const STUDENT_1_ADDRESS: string;
+    export const STUDENT_2_ADDRESS: string;
+    export const SEPOLIA_URL: string;
+    export const SMART_CONTRACT_ADDRESS: string;
+}
