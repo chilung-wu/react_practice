7bae51779c712575e88814934fdeea030cef26b6
chilung <chilung.wu.cs10@nycu.edu.tw>
2024-02-26 22:05:25 +0800

    connect_to_metamask_wallet

--- MobilePasswordManager/App.js
+++ MobilePasswordManager/App.js
@@ -1,8 +1,41 @@
+import { StatusBar } from 'expo-status-bar'
+import { Modal, Pressable, StyleSheet, View, TextInput, Button, FlatList, Text, TouchableOpacity, Alert } from 'react-native'
+import '@walletconnect/react-native-compat'
+import { WagmiConfig, useAccount } from 'wagmi'
+import { sepolia} from 'viem/chains'
+import { createWeb3Modal, defaultWagmiConfig, Web3Modal, W3mButton} from '@web3modal/wagmi-react-native'
+import {PROJECT_ID} from "@env"
 import React, { useState, useEffect } from 'react';
-import { Modal, Pressable, StyleSheet, View, TextInput, Button, FlatList, Text, TouchableOpacity, Alert } from 'react-native';
 import * as SecureStore from 'expo-secure-store';
 import CryptoJS from "rn-crypto-js";
 import * as Clipboard from 'expo-clipboard'
+
+// 1. Get projectId at https://cloud.walletconnect.com
+const projectId = PROJECT_ID
+console.log('projectId', PROJECT_ID)
+
+// 2. Create config
+const metadata = {
+  name: 'Web3Modal RN',
+  description: 'Web3Modal RN Example',
+  url: 'https://web3modal.com',
+  icons: ['https://avatars.githubusercontent.com/u/37784886'],
+  redirect: {
+    native: 'YOUR_APP_SCHEME://',
+    universal: 'YOUR_APP_UNIVERSAL_LINK.com'
+  }
+}
+
+const chains = [sepolia]
+
+const wagmiConfig = defaultWagmiConfig({ chains, projectId, metadata })
+
+// 3. Create modal
+createWeb3Modal({
+  projectId,
+  chains,
+  wagmiConfig
+})
 
 export default function App() {
   const [website, setWebsite] = useState('');
@@ -229,6 +262,14 @@ export default function App() {
       ) : (
         <Text style={[styles.text]}>masterPassword is {masterPassword}</Text>
       )}
+      <WagmiConfig config={wagmiConfig}>
+        <View style={styles.container}>
+          <W3mButton balance='show'/>
+          <StatusBar style="auto" />
+        </View>
+      <Web3Modal />
+      </WagmiConfig>
+      <Text>{"\n"}</Text>
       <TextInput style={[styles.input, styles.itemTextStyle]} placeholder="Website URL" value={website} onChangeText={setWebsite} />
       <TextInput style={[styles.input, styles.itemTextStyle]} placeholder="Username" value={username} onChangeText={setUsername} />
       <TextInput style={[styles.input, styles.itemTextStyle]} placeholder="Password" value={password} onChangeText={setPassword} secureTextEntry />
