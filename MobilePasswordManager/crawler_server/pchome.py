8c784322a1c16e41ec6c029a227ef9312b703ba5
chilung <chilung.wu.cs10@nycu.edu.tw>
2024-03-02 22:09:12 +0800

    finish_basic_order_collect. 'prodName_name' and 'link_logistics' is hided until click 'font.winlink.detailSwitchIcon' click_detail_switch_icon() click 'click_detail_switch_icon' then show order infomation. crawl local index.html for test and debug 'expected_conditions' replace 'time.sleep' to be more efficiency. add 'options.add_experimental_option("detach", True)' keep browser open  after program finish.

--- MobilePasswordManager/crawler_server/pchome.py
+++ MobilePasswordManager/crawler_server/pchome.py
@@ -9,32 +9,31 @@ from selenium.webdriver.support import expected_conditions as EC
 import time
 load_dotenv()
 login_url = "https://ecvip.pchome.com.tw/login/v3/login.htm"
-# test_url = "https://ecvip.pchome.com.tw/login/v3/login.htm"
 order_url = "https://ecvip.pchome.com.tw/web/order/all"
 home_url = "https://ecvip.pchome.com.tw/"
 username = os.getenv("username")
 password = os.getenv("password")
-
+# local_html_path = 'file://' + os.path.abspath("/home/chilung/Desktop/test_garbage/20240126_react_practice/react_practice/MobilePasswordManager/crawler_server/index.html")
 The_End_page = False
-
-# 訂單編號 日期 訂單狀態 總價 付款 配送狀態
-
-
-# problemlist_url = "https://leetcode.com/problemset/all/"
 
 options = webdriver.ChromeOptions()
 options.binary_location = "/usr/bin/google-chrome-stable"
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
+options.add_experimental_option("detach", True)
 
 # # Adding headless mode to run the browser in the background
-options.add_argument("--headless")
+# options.add_argument("--headless")
 
 
 def main():
     driver = webdriver.Chrome(options=options)
 
     try:
+        '''
+        # driver.get(local_html_path)
+        '''
+
         driver.get(login_url)
         usernameInput = WebDriverWait(driver, 10).until(
             EC.presence_of_element_located((By.ID, "loginAcc"))
@@ -61,24 +60,16 @@ def main():
         )
 
         driver.get(order_url)
-        # WebDriverWait(driver, 10).until(
-        #     EC.presence_of_element_located((By.ID, "listOrder"))
-        # )
 
-        time.sleep(3)  # load order page
+        # time.sleep(3)  # load order page
+        # expected_conditions
+        WebDriverWait(driver, 10).until(
+            EC.visibility_of_element_located((By.ID, "listOrder"))
+        )
 
-        # Initialize an empty list to store dictionaries
         orders = []
         The_End_page = False
 
-        # Define a function to check if the "下一頁" text is present
-        def has_next_page():
-            try:
-                driver.find_element(By.XPATH, '//span[@id="next_page"]/a[text()="下一頁"]').click()
-                time.sleep(2)
-                return True
-            except:
-                return False
 
         def has_next_page_and_click(driver):
             try:
@@ -94,47 +85,91 @@ def main():
             except:
                 return False
 
+        def extract_additional_info(order_id, driver):
+            # 假設產品名稱和物流資訊位於相同的div中，並且這個div的id是根據訂單編號生成的
+            div_id = f"ODD{order_id}B"  # 根據實際情況調整
+            product_name = ""
+            logistics_info = "None"
+            # logistics_info = ""
 
-        def extract_order_info(table):
+            try:
+                # 定位到包含產品名稱和物流資訊的div
+                detail_div = driver.find_element(By.ID, div_id)
+                # 提取產品名稱
+                product_name_element = detail_div.find_element(By.CSS_SELECTOR, ".prodName_name")
+                product_name = product_name_element.text
+                # 提取物流資訊
+                try:
+                    logistics_info_element = detail_div.find_element(By.CSS_SELECTOR, ".link_logistics")
+                    logistics_info = logistics_info_element.text
+                except:
+                    pass
+            except Exception as e:
+                # pass
+                print(f"Error extracting additional info for order {order_id}: {e}")
+
+            return product_name, logistics_info
+
+        def extract_order_info(table, driver):
             orders = []
-            rows = table.find_elements(By.CSS_SELECTOR, "tr.content_tr")
-            for row in rows:
-                cells = row.find_elements(By.CSS_SELECTOR, "td")
-                if len(cells) < 5:
-                    continue
-                order_info = {
-                    '訂單編號': cells[0].text,
-                    '日期': cells[1].text,
-                    '訂單狀態': cells[2].text,
-                    '總價': cells[3].text,
-                    '付款': cells[4].text,
-                    '配送狀態': cells[5].text,
-                }
-                orders.append(order_info)
+            row = table.find_element(By.CSS_SELECTOR, "tr.content_tr")
+            cells = row.find_elements(By.CSS_SELECTOR, "td")
+            if len(cells) < 5:
+                return orders
+            
+            # 提取產品名稱和物流資訊
+            order_id = cells[0].text
+            product_name, logistics_info = extract_additional_info(order_id, driver)
+            if logistics_info == "None":
+                return orders
+
+            # order_info = {}
+            order_info = {
+                '訂單編號': cells[0].text,
+                '日期': cells[1].text,
+                '訂單狀態': cells[2].text,
+                '總價': cells[3].text,
+                '付款': cells[4].text,
+                '配送狀態': cells[5].text,
+                '產品名稱': product_name,
+                '物流資訊': logistics_info
+            }
+
+            orders.append(order_info)
+
             return orders
 
+        def click_detail_switch_icon(driver):
+            WebDriverWait(driver, 10).until(
+                EC.presence_of_all_elements_located((By.CSS_SELECTOR, "font.winlink.detailSwitchIcon"))
+            )
+
+            detail_buttons = driver.find_elements(By.CSS_SELECTOR, "font.winlink.detailSwitchIcon")
+
+            for detail_button in detail_buttons:
+                driver.execute_script("arguments[0].click();", detail_button)
+            
+            # check .prodName_name is visible
+            WebDriverWait(driver, 10).until(
+                EC.visibility_of_element_located((By.CSS_SELECTOR, ".prodName_name"))
+            )
 
         Has_Next_page = True
 
         while Has_Next_page:
-            # Find the div with id "listOrder"
+            click_detail_switch_icon(driver)
             list_order_div = driver.find_element(By.ID, "listOrder")
-            # Find all the tables with class "order_table_y" within the div
             tables = list_order_div.find_elements(By.CSS_SELECTOR, "table.order_table_y")
-            # Loop through each table and extract information
             for table in tables:
-                orders += extract_order_info(table)
+                orders += extract_order_info(table, driver)
             
             # Check if the "下一頁" link exists
             Has_Next_page = has_next_page_and_click(driver)
-            # Has_Next_page = has_next_page()
             
         # Print the list of orders
         print(f'Total numbers of orders: {len(orders)}')
         for order in orders:
             print(order)
-            
-        # ... rest of your code ...
 
     except Exception as e:
         print("An error occurred:", e)
